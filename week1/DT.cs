c2a7274b38639e2ad602c8824a30ffa0552b6c44
Kangdo <166014987+DohyunKang@users.noreply.github.com>
2024-08-23 16:47:32 +0900

    Update DT.cs

--- week1/DT.cs
+++ week1/DT.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-//using System.Threading.Task;
 using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
@@ -13,7 +12,7 @@ using NationalInstruments.UI.WindowsForms;
 namespace week1
 {
     public class DT : IDisposable
-    { 
+    {
         private Task diTask = new Task();
         private Task doTask = new Task();
         private UInt32 data;
@@ -27,20 +26,25 @@ namespace week1
         Led led;
         WaveformGraph wfg;
 
+        // 이전 Tick 시간을 저장하기 위한 변수
+        private DateTime lastTickTime;
+
         public DT(int index, Led led, WaveformGraph wfg)
         {
             this.index = index;
-            this.led = led;            
+            this.led = led;
             this.wfg = wfg;
 
             SetUpDigitalRead(index);
             SetUpDigitalWrite(index);
 
+            // 초기화 시점에 lastTickTime을 현재 시간으로 설정
+            lastTickTime = DateTime.Now;
+
             timer = new Timer();
-            timer.Interval = 100;
+            timer.Interval = 100; // 타이머 간격 설정 (밀리초)
             timer.Tick += ReadSignal;
             timer.Start();
-               
         }
 
         public void Dispose()
@@ -48,12 +52,10 @@ namespace week1
             if (timer != null)
             {
                 timer.Dispose();
-
             }
             if (diTask != null)
             {
-               diTask.Dispose();
-
+                diTask.Dispose();
             }
             if (doTask != null)
             {
@@ -81,21 +83,19 @@ namespace week1
 
         private void SetUpDigitalRead(int index)
         {
-           diTask = new Task();
+            diTask = new Task();
             try
             {
-               diTask.DIChannels.CreateChannel(
-                       string.Format("Dev1/port1/line{0}", index),
-                       string.Format("DI{0}", (index+1)),
-                       ChannelLineGrouping.OneChannelForAllLines
-                       );
+                diTask.DIChannels.CreateChannel(
+                    string.Format("Dev1/port1/line{0}", index),
+                    string.Format("DI{0}", (index + 1)),
+                    ChannelLineGrouping.OneChannelForAllLines
+                );
             }
             catch (Exception)
             {
-
                 Application.Exit();
             }
-
 
             reader = new DigitalSingleChannelReader(diTask.Stream);
         }
@@ -103,14 +103,13 @@ namespace week1
         private void SetUpDigitalWrite(int index)
         {
             doTask = new Task();
-
             try
             {
                 doTask.DOChannels.CreateChannel(
-                        string.Format("Dev1/port0/line{0}", index),
-                        string.Format("DO{0}", (index+1)),
-                        ChannelLineGrouping.OneChannelForAllLines
-                        );
+                    string.Format("Dev1/port0/line{0}", index),
+                    string.Format("DO{0}", (index + 1)),
+                    ChannelLineGrouping.OneChannelForAllLines
+                );
             }
             catch (Exception)
             {
@@ -120,20 +119,24 @@ namespace week1
             writer = new DigitalSingleChannelWriter(doTask.Stream);
         }
 
-
         private void ReadSignal(object sender, EventArgs e)
         {
             try
             {
+                // 현재 시간을 가져와 이전 Tick 시간과 비교
+                DateTime currentTime = DateTime.Now;
+                double elapsedSeconds = (currentTime - lastTickTime).TotalSeconds;
+
                 data = reader.ReadSingleSamplePortUInt32();
-                //AnalyzeSignal(data);
-                AnalyzeSignal(index, data);
+                AnalyzeSignal(index, data, elapsedSeconds);
+
+                // 현재 시간을 마지막 Tick 시간으로 업데이트
+                lastTickTime = currentTime;
             }
             catch (Exception)
             {
                 Application.Exit();
             }
-
         }
 
         public void WriteSignal(bool switchValue)
@@ -141,65 +144,26 @@ namespace week1
             writer.WriteSingleSampleSingleLine(true, switchValue);
         }
 
-        /*public void AnalyzeSignal(UInt32 data)
-        {
-            if (data != 0)
-            {
-                wfg.PlotYAppend(Convert.ToDouble(true), 0.1);              
-                led.Value = true;
-            }
-            else
-            {
-                wfg.PlotYAppend(Convert.ToDouble(false), 0.1);
-                led.Value = false;
-            }
-        }*/
-
         // wfg에 아날로그 입력 값 플로팅
-        public void AnalyzeSignal(int index, UInt32 data)
+        public void AnalyzeSignal(int index, UInt32 data, double elapsedSeconds)
         {
             // 동적으로 아날로그 입력 채널 이름 생성
             string channelName = string.Format("Dev1/ai{0}", index);
-            string.Format("AI{0}", (index + 1));
 
             // 생성된 채널 이름을 사용하여 아날로그 입력 값 읽기
             double voltageValue = ReadAnalogInput(channelName);
 
+            // 읽어온 전압 값을 그래프에 추가하고, LED 상태 변경
             if (data != 0)
             {
-                // 읽어온 전압 값을 그래프에 추가
-                wfg.PlotYAppend(voltageValue, 0.1);
+                wfg.PlotYAppend(voltageValue, elapsedSeconds); // 실제 경과 시간을 그래프에 반영
                 led.Value = true;
             }
             else
             {
-                // 읽어온 전압 값을 그래프에 추가
-                //wfg.PlotYAppend(Convert.ToDouble(false), 0.1);
-                wfg.PlotYAppend(voltageValue, 0.1);
+                wfg.PlotYAppend(voltageValue, elapsedSeconds); // 실제 경과 시간을 그래프에 반영
                 led.Value = false;
             }
         }
-
-
-
-        /*public void AnalyzeSignal(UInt32 data)
-        {
-            if (data != 0)
-            {
-                for (double i = 0; i <= 10; i += 0.1)
-                {
-                    wfg.PlotYAppend(i, 0.1);
-                }
-                led.Value = true;
-            }
-            else
-            {
-                for (double i = 10; i >= 0; i -= 0.1)
-                {
-                    wfg.PlotYAppend(i, 0.1);
-                }
-                led.Value = false;
-            }
-        }*/
     }
 }
